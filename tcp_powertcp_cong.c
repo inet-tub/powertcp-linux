890e1913e10a9dd0026a1c0de7e5ae3e4a635d19
Jörn-Thorben Hinz <jthinz@mailbox.tu-berlin.de>
2022-03-15 15:44:03 +0100

    Drop pseudocode

--- tcp_powertcp_cong.c
+++ tcp_powertcp_cong.c
@@ -26,86 +26,6 @@
 #include <linux/units.h>
 #include <net/tcp.h>
 
-/*
-# PowerTCP algorithm
-
-# ack contains an INT header with
-# sequence of per-hop egress port
-# meta-data accessed as ack.H[i]
-
-Input: ack, prevInt
-Output: cwnd, rate
-
-procedure NEW_ACK (ack):
-	cwnd_old = GET_CWND(ack.seq)
-	normPower = NORM_POWER(ack)
-	UPDATE_WINDOW(normPower, cwnd_old)
-	rate = cwnd / τ
-	prevInt = ack.H
-	UPDATE_OLD(cwnd, ack.seq)
-
-function NORM_POWER(ack):
-	Γ_norm = 0
-	for each egress port i on the path do
-		dt = ack.H[i].ts - prevInt[i].ts
-		q̇  = (ack.H[i].qlen - prevInt[i].qlen) / dt      # dq / dt
-		λ = (ack.H[i].txBytes - prevInt[i].txBytes) / dt
-		λ = q̇ + µ                                     # λ: Current
-		BDP = ack.H[i].b * τ
-		ν = ack.H[i].qlen + BDP
-		Γ = λ * ν
-		e = ack.H[i].b^2 * τ
-		Γ'_norm = Γ' / e                              # Γ'_norm: Normalized power
-		if Γ' > Γ_norm then
-			Γ_norm = Γ'
-			∆t = dt
-		end if
-	end for
-	Γ_smooth = (Γ_smooth * (τ - ∆t) + (Γ_norm * ∆t)) / τ # Smoothing
-	return Γ_smooth
-
-function UPDATE_WINDOW(power, ack):
-	cwnd = γ * (cwnd_old / normPower + β) + (1 - γ) * cwnd
-	                                                  # γ : EWMA parameter
-	                                                  # β: Additive Increase
-	return cwnd
-*/
-
-/*
-# θ-PowerTCP (w/o switch support) algorithm
-
-# t_c is the timestamp when ACK is received
-
-Input: ack
-Output: cwnd, rate
-
-procedure NEW_ACK(ack):
-	cwnd_old = GET_CWND(ack.seq)
-	normPower = NORM_POWER(ack)
-	UPDATE_WINDOW(normPower, cwnd_old)
-	rate = cwnd / τ
-	prevRTT = RTT
-	t_c_prev = t_c
-	UPDATE_OLD(cwnd, ack.seq)
-
-function NORM_POWER(ack):
-	dt = t_c - t_c_prev
-	θ̇ = (RTT - prevRTT) / dt                    # dRTT / dt
-	Γ_norm = ((θ̇  + 1) * RTT) / τ                # Γ_norm: Normalized
-	Γ_smooth = (Γ_smooth * (τ - ∆t) + (Γ_norm * ∆t)) / τ
-	return Γ_smooth
-
-function UPDATE_WINDOW(power, ack):
-	if ack.seq < lastUpdated then               # per RTT
-		return cwnd
-	end if
-	cwnd = γ * (cwnd_old / normPower + β) + (1 - γ) * cwnd
-	                                            #. γ : EWMA parameter
-	                                            #. β: Additive Increase
-	lastUpdated = snd_nxt
-	return cwnd
-*/
-
 enum powertcp_variant {
 	POWERTCP_POWERTCP = 0,
 	POWERTCP_RTTPOWERTCP = 1,
@@ -527,8 +447,6 @@ static void powertcp_cong_control(struct sock *sk, const struct rate_sample *rs)
 
 	base_rtt_us = base_rtt(sk, rs);
 
-	// NOTE: Mostly based on the pseudo-code, might actually be done slightly
-	// different in real code:
 	cwnd_old = get_cwnd(sk);
 	norm_power = ca->norm_power(sk, rs, base_rtt_us);
 	cwnd = ca->update_window(sk, cwnd_old, norm_power);
