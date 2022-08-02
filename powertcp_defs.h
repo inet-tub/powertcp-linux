0db79ea21b306c84e89b0f95d95de211cb57cb15
Jörn-Thorben Hinz <jthinz@mailbox.tu-berlin.de>
2022-08-02 17:12:53 +0200

    Avoid an "initializer element is not constant" error with gcc before 8.1

--- powertcp_defs.h
+++ powertcp_defs.h
@@ -10,11 +10,17 @@ static const unsigned long fallback_host_bw = 1000; /* Mbit/s */
 static const unsigned long gamma_scale = (1UL << 10);
 static const unsigned long power_scale = (1UL << 16);
 
-static const int default_base_rtt = -1; /* us */
-static const int default_beta = -1; /* Number of packets */
-static const int default_expected_flows = 10;
-static const int default_gamma = 0.9 * gamma_scale;
-static const int default_hop_bw = 1000; /* Mbit/s */
-static const int default_host_bw = 1000; /* Mbit/s */
+/* Avoid an "initializer element is not constant" error with gcc before 8.1 by
+ * using an enum instead of static const variables. No, I don't want to use
+ * macros for constants here :-)
+ */
+enum {
+	default_base_rtt = -1, /* us */
+	default_beta = -1, /* Number of packets */
+	default_expected_flows = 10,
+	default_gamma = 921, /* ~= 0.9 * gamma_scale */
+	default_hop_bw = 1000, /* Mbit/s */
+	default_host_bw = 1000, /* Mbit/s */
+};
 
 #endif /* POWERTCP_DEFS_H */
